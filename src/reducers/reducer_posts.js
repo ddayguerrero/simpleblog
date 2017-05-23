d3a0386275fcad79ffa56169e123268b3352cf59
Darrel-Day Guerrero <ddayy.guerrero@hotmail.com>
2017-05-23 00:39:51 -0400

    Fix action creator typo, map payload data...

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,6 +4,7 @@ import { FETCH_POSTS } from '../actions';
 export default function(state = {}, action){
     switch(action.type){
         case FETCH_POSTS:{
+            console.log(action.payload.data);
             return _.mapKeys(action.payload.data, 'id');
         }
         default:
